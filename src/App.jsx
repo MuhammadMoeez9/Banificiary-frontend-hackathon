bc096a65834707262a5f8956f48e55c58504e469
Muhammad Moeez <moeezshkh9777@gmail.com>
2025-01-25 18:50:29 -0800

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/signup' element={<Signup></Signup>}></Route>
-        <Route path='adminpanel' element={<AdminPanel></AdminPanel>}></Route>
+        <Route path='/adminpanel' element={<AdminPanel></AdminPanel>}></Route>
         <Route path='slip/:token' element={<Slip></Slip>}></Route>
         <Route path='/department-manger' element={<DepartmentManger></DepartmentManger>}></Route>
       </Routes>
